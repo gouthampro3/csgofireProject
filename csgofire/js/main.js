91b40377664aa67459643d9a072913ae0075a663
gouthampro3 <gouthampro3@gmail.com>
2017-08-05 03:43:52 +0530

    Add files via upload

---
+++ csgofire/js/main.js
@@ -0,0 +1,71 @@
+jQuery(document).ready(function(){
+	//cache DOM elements
+	var mainContent = $('.cd-main-content'),
+		header = $('.cd-main-header'),
+		sidebar = $('.cd-side-nav'),
+		sidebarTrigger = $('.cd-nav-trigger'),
+		topNavigation = $('.cd-top-nav'),
+		searchForm = $('.cd-search'),
+		accountInfo = $('.account');
+
+	//on resize, move search and top nav position according to window width
+	var resizing = false;
+	moveNavigation();
+	$(window).on('resize', function(){
+		if( !resizing ) {
+			(!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
+			resizing = true;
+		}
+	});
+
+	//mobile only - open sidebar when user clicks the hamburger menu
+	sidebarTrigger.on('click', function(event){
+		event.preventDefault();
+		$([sidebar, sidebarTrigger]).toggleClass('nav-is-visible');
+	});
+
+	function checkMQ() {
+		//check if mobile or desktop device
+		return window.getComputedStyle(document.querySelector('.cd-main-content'), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "");
+	}
+
+	function moveNavigation(){
+  		var mq = checkMQ();
+
+        if ( mq == 'mobile' && topNavigation.parents('.cd-side-nav').length == 0 ) {
+        	detachElements();
+			topNavigation.appendTo(sidebar);
+			searchForm.removeClass('is-hidden').prependTo(sidebar);
+		} else if ( ( mq == 'tablet' || mq == 'desktop') &&  topNavigation.parents('.cd-side-nav').length > 0 ) {
+			detachElements();
+			searchForm.insertAfter(header.find('.cd-logo'));
+			topNavigation.appendTo(header.find('.cd-nav'));
+		}
+		checkSelected(mq);
+		resizing = false;
+	}
+
+	function detachElements() {
+		topNavigation.detach();
+		searchForm.detach();
+	}
+
+	function checkSelected(mq) {
+		//on desktop, remove selected class from items selected on mobile/tablet version
+		if( mq == 'desktop' ) $('.has-children.selected').removeClass('selected');
+	}
+
+	function checkScrollbarPosition() {
+		var mq = checkMQ();
+
+		if( mq != 'mobile' ) {
+			var sidebarHeight = sidebar.outerHeight(),
+				windowHeight = $(window).height(),
+				mainContentHeight = mainContent.outerHeight(),
+				scrollTop = $(window).scrollTop();
+
+			( ( scrollTop + windowHeight > sidebarHeight ) && ( mainContentHeight - sidebarHeight != 0 ) ) ? sidebar.addClass('is-fixed').css('bottom', 0) : sidebar.removeClass('is-fixed').attr('style', '');
+		}
+		scrolling = false;
+	}
+});
